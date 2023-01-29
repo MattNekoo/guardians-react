93281eefc36daf41050e589700c120613ff114d2
Mateus Nascimento <mateusnl.vg@gmail.com>
2023-01-29 15:48:53 -0300

    fetchDocuments firebase

--- src/Pages/Inicio/Inicio.js
+++ src/Pages/Inicio/Inicio.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import Zerados from './Zerados'
+import PostDetail from '../../components/PostDetail'
+import "../../hooks/useFetchDocuments";
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
 
 const Inicio = () => {
+    const { documents: posts, loading } = useFetchDocuments('posts');
 
     return (
         <div>
-            <Zerados />
-        </div>    
+            {/* <Zerados /> */}
+            {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+        </div>
     )
 }
 
